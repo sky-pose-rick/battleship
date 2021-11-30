cfbcea4017106fb2948ae925d378c651d3e01b1f
Richard Cranwell <rickycranwell@gmail.com>
2021-11-30 17:09:01 -0600

    change variable names on player tests to be clearer

--- src/player.test.js
+++ src/player.test.js
@@ -1,20 +1,18 @@
 import playerFactory from './player';
 import gameboardFactory from './gameboard';
 
-let player1;
-let board1;
-let player2;
+let enemyBoard;
+let player;
 let targetID;
 let targetList;
 let targetMock;
 
 describe('testing a mock targeting function', () => {
   beforeAll(() => {
-    player1 = playerFactory();
-    board1 = gameboardFactory();
-    player2 = playerFactory();
+    enemyBoard = gameboardFactory();
+    player = playerFactory();
 
-    board1.placeShip({
+    enemyBoard.placeShip({
       length: 4,
       row: 2,
       col: 3,
@@ -31,28 +29,28 @@ describe('testing a mock targeting function', () => {
   });
 
   it('player can declare an attack', async () => {
-    await player2.takeTurn(board1, targetMock);
-    expect(board1.getTile(2, 3).hit).toBeTruthy();
-    expect(board1.getTile(2, 4).hit).toBeFalsy();
+    await player.takeTurn(enemyBoard, targetMock);
+    expect(enemyBoard.getTile(2, 3).hit).toBeTruthy();
+    expect(enemyBoard.getTile(2, 4).hit).toBeFalsy();
   });
 
   it('targeting is retried on invalid targets', async () => {
-    await player2.takeTurn(board1, targetMock);
-    expect(board1.getTile(7, 7).miss).toBeTruthy();
+    await player.takeTurn(enemyBoard, targetMock);
+    expect(enemyBoard.getTile(7, 7).miss).toBeTruthy();
   });
 
   it('test AI targeting', async () => {
     // last hit was 2,3
     // have AI target neighboring cell left, up, right, down after a hit
     // expect next 4 targeted cells to be [2,2]miss[1,3]miss[2,4]hit[1,4]miss
-    const targetFunc = player2.aiTarget;
-    await player2.takeTurn(board1, targetFunc);
-    expect(board1.getTile(2, 2).miss).toBeTruthy();
-    await player2.takeTurn(board1, targetFunc);
-    expect(board1.getTile(1, 3).miss).toBeTruthy();
-    await player2.takeTurn(board1, targetFunc);
-    expect(board1.getTile(2, 4).hit).toBeTruthy();
-    await player2.takeTurn(board1, targetFunc);
-    expect(board1.getTile(1, 4).miss).toBeTruthy();
+    const targetFunc = player.aiTarget;
+    await player.takeTurn(enemyBoard, targetFunc);
+    expect(enemyBoard.getTile(2, 2).miss).toBeTruthy();
+    await player.takeTurn(enemyBoard, targetFunc);
+    expect(enemyBoard.getTile(1, 3).miss).toBeTruthy();
+    await player.takeTurn(enemyBoard, targetFunc);
+    expect(enemyBoard.getTile(2, 4).hit).toBeTruthy();
+    await player.takeTurn(enemyBoard, targetFunc);
+    expect(enemyBoard.getTile(1, 4).miss).toBeTruthy();
   });
 });
