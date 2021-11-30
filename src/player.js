8d7cd35615f2ac40afbb18670b11c41a3bf116d8
Richard Cranwell <rickycranwell@gmail.com>
2021-11-30 16:59:51 -0600

    add decision making for computer players

--- src/player.js
+++ src/player.js
@@ -3,20 +3,32 @@ import gameboardFactory from './gameboard';
 const playerFactory = () => {
   const board = gameboardFactory();
 
+  const hitStack = [];
+  let searchCount = 0;
+  const searchOffsets = [[0, -1], [-1, 0], [0, 1], [1, 0]];
+
   const getBoard = () => board;
 
-  async function takeTurn(targetFunc) {
+  const takeTurn = async function takeTurn(otherPlayer, targetFunc) {
     let validTarget = false;
     let row;
     let col;
+    const otherBoard = otherPlayer.getBoard();
     while (!validTarget) {
       // eslint-disable-next-line no-await-in-loop
       [row, col] = await targetFunc();
-      validTarget = board.isValidTarget(row, col);
+      validTarget = otherBoard.isValidTarget(row, col);
     }
 
-    return Promise.resolve('good turn');
-  }
+    const hit = otherBoard.receiveAttack(row, col);
+    if (hit) {
+      hitStack.push([row, col]);
+      searchCount = 0;
+    }
+    // console.log('targeted', row, col);
+
+    return Promise.resolve({ row, col, hit });
+  };
 
   const randomTarget = () => {
     const row = Math.random(0, 10);
@@ -24,7 +36,27 @@ const playerFactory = () => {
     return Promise.resolve([row, col]);
   };
 
-  return { getBoard, takeTurn, randomTarget };
+  const aiTarget = () => {
+    if (searchCount === 4) {
+      hitStack.pop();
+      searchCount = 0;
+    }
+    if (hitStack.length === 0) {
+      return Promise.resolve(randomTarget);
+    }
+    let row;
+    let col;
+    [row, col] = hitStack[hitStack.length - 1];
+    const offset = searchOffsets[searchCount];
+    row += offset[0];
+    col += offset[1];
+    searchCount += 1;
+    return Promise.resolve([row, col]);
+  };
+
+  return {
+    getBoard, takeTurn, randomTarget, aiTarget,
+  };
 };
 
 export default playerFactory;
--- src/player.test.js
+++ src/player.test.js
@@ -1,23 +1,27 @@
 import playerFactory from './player';
 
-let player;
-let board;
+let player1;
+let board1;
+let player2;
+let board2;
 let targetID;
 let targetList;
 let targetMock;
 
 it('player owns a board', () => {
-  player = playerFactory();
-  board = player.getBoard();
-  expect(board).toBeTruthy();
+  player1 = playerFactory();
+  board1 = player1.getBoard();
+  expect(board1).toBeTruthy();
 });
 
 describe('testing a mock targeting function', () => {
   beforeAll(() => {
-    player = playerFactory();
-    board = player.getBoard();
+    player1 = playerFactory();
+    board1 = player1.getBoard();
+    player2 = playerFactory();
+    board2 = player2.getBoard();
 
-    board.placeShip({
+    board1.placeShip({
       length: 4,
       row: 2,
       col: 3,
@@ -25,7 +29,7 @@ describe('testing a mock targeting function', () => {
     });
 
     targetID = 0;
-    targetList = [[2, 3], [2, 3], [3, 3]];
+    targetList = [[2, 3], [2, 3], [7, 7], [8, 8]];
     targetMock = () => {
       const nextTarget = targetList[targetID];
       targetID += 1;
@@ -33,12 +37,29 @@ describe('testing a mock targeting function', () => {
     };
   });
 
-  it('player can declare an attack', () => player.takeTurn(targetMock).then((data) => {
-    expect(board.getTile(2, 3).hit).toBeTruthy;
-    expect(board.getTile(2, 4).hit).toBeFalsy;
-  }));
+  it('player can declare an attack', async () => {
+    await player2.takeTurn(player1, targetMock);
+    expect(board1.getTile(2, 3).hit).toBeTruthy();
+    expect(board1.getTile(2, 4).hit).toBeFalsy();
+  });
 
-  it('targeting is retried on invalid targets', () => player.takeTurn(targetMock).then((data) => {
-    expect(board.getTile(3, 3).miss).toBeTruthy;
-  }));
+  it('targeting is retried on invalid targets', async () => {
+    await player2.takeTurn(player1, targetMock);
+    expect(board1.getTile(7, 7).miss).toBeTruthy();
+  });
+
+  it('test AI targeting', async () => {
+    // last hit was 2,3
+    // have AI target neighboring cell left, up, right, down after a hit
+    // expect next 4 targeted cells to be [2,2]miss[1,3]miss[2,4]hit[1,4]miss
+    const targetFunc = player2.aiTarget;
+    await player2.takeTurn(player1, targetFunc);
+    expect(board1.getTile(2, 2).miss).toBeTruthy();
+    await player2.takeTurn(player1, targetFunc);
+    expect(board1.getTile(1, 3).miss).toBeTruthy();
+    await player2.takeTurn(player1, targetFunc);
+    expect(board1.getTile(2, 4).hit).toBeTruthy();
+    await player2.takeTurn(player1, targetFunc);
+    expect(board1.getTile(1, 4).miss).toBeTruthy();
+  });
 });
